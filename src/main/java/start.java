f6794b1448f888d7cfcecdf550f0189c71efd5c3
Eugene Smyrnov AcerE5 <helldaf@yandex.ru>
2016-11-07 13:23:29 +0200

    Inserting JDBC

--- src/main/java/start.java
+++ src/main/java/start.java
@@ -1,8 +1,32 @@
-/**
- * Created by Evgeniy on 20.10.2016.
- */
+import java.sql.*;
+
 public class start {
     public static void main(String[] arg){
+        Connection conn = null;
 
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/test", "root", "ttipopu");
+
+            Statement sqlState = conn.createStatement();
+
+            String selectStuff = "Select login from accounts";
+
+            ResultSet rows = sqlState.executeQuery(selectStuff);
+
+            while(rows.next()){
+                System.out.println(rows.getString("login"));
+            }
+        }
+
+        catch (SQLException sqlEx) {
+            System.out.println("SQLException: " + sqlEx.getMessage());
+            System.out.println("VendorError: " + sqlEx.getErrorCode()  );
+        }
+
+        catch (ClassNotFoundException classEx){
+            classEx.printStackTrace();
+        }
     }
 }
