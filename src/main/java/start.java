aad3b63ae37a19f41430e8925abea2a938b36643
Eugene Smyrnov AcerE5 <helldaf@yandex.ru>
2016-11-09 16:03:54 +0200

    Creating inserting method.

--- src/main/java/start.java
+++ src/main/java/start.java
@@ -1,29 +1,76 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.sql.*;
 
 public class start {
-    public static void main(String[] arg){
-        Connection conn;
+    public static void main(String[] arg) throws IOException{
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            System.out.print("Enter login: ");
+            String log = reader.readLine();
 
+            System.out.print("Enter email: ");
+            String emai = reader.readLine();
+
+            System.out.print("Enter password: ");
+            String pass = reader.readLine();
+
+            System.out.print("Enter phone number: ");
+            String phone = reader.readLine();
+
+            System.out.print("Enter First Name: ");
+            String firstName = reader.readLine();
+
+            System.out.print("Enter Last Name: ");
+            String lastName = reader.readLine();
+
+            System.out.print("Enter Street: ");
+            String stree = reader.readLine();
+
+            System.out.print("Enter City: ");
+            String cit = reader.readLine();
+
+            System.out.print("Enter day of birth (YYYY-MM-DD): ");
+            String dob = reader.readLine();
+
+            System.out.print("Enter sex (M or F): ");
+            String gender = reader.readLine();
+
+            insertMethod(log, emai, pass, phone, firstName, lastName, stree, cit, dob, gender);
+
+            System.out.print("To exit the program type: Exit ");
+            if(reader.readLine().equals("Exit"))
+                break;
+
+
+        }
+    }
+
+    private static void insertMethod(String log, String emai, String pass, String phone, String firstName, String lastName,
+                      String stree, String cit, String dob, String gender)
+    {
         try{
+            Connection conn;
             Class.forName("com.mysql.jdbc.Driver");
-
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/test", "root", "ttipopu");
-
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/test?autoReconnect=true&useSSL=false", "root", "ttipopu");
             Statement sqlState = conn.createStatement();
 
+            sqlState.executeUpdate("INSERT INTO accounts " + "VALUES ('"+ log +"', '"+ emai +"', '"+ pass +"'," +
+                    " '"+ phone +"', '"+ firstName +"', '"+ lastName +"', '"+ stree +"', '"+ cit +"'," +
+                    " '"+ dob +"', '"+ gender +"', null)");
+
             String selectStuff = "SELECT login, email, password FROM accounts";
-
             ResultSet rows = sqlState.executeQuery(selectStuff);
-
             while(rows.next()){
                 System.out.println("Login: " + rows.getString("login") + "    Email: " + rows.getString("email") +
                         "    Password: " + rows.getString("password"));
             }
 
-            sqlState.executeUpdate("INSERT INTO accounts "+ "VALUES ('gafer', 'gafer@ya.ru', 'chashpasw', '0933994443', 'Huge', 'Mangus', 'Duglas', 'Krakow', '1996-04-23', 'M', null)");
         }
 
         catch (SQLException sqlEx) {
+            System.out.println("Error. Incorrectly entered data!");
             System.out.println("SQLException: " + sqlEx.getMessage());
             System.out.println("VendorError: " + sqlEx.getErrorCode()  );
         }
@@ -31,5 +78,6 @@ public class start {
         catch (ClassNotFoundException classEx){
             classEx.printStackTrace();
         }
+
     }
 }
