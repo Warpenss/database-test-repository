5ede72383f98ec02797b13acfb64493a1add3f38
Eugene Smyrnov AcerE5 <helldaf@yandex.ru>
2016-11-09 14:22:28 +0200

    Inserting JDBC

--- src/main/java/start.java
+++ src/main/java/start.java
@@ -2,7 +2,7 @@ import java.sql.*;
 
 public class start {
     public static void main(String[] arg){
-        Connection conn = null;
+        Connection conn;
 
         try{
             Class.forName("com.mysql.jdbc.Driver");
@@ -11,13 +11,16 @@ public class start {
 
             Statement sqlState = conn.createStatement();
 
-            String selectStuff = "Select login from accounts";
+            String selectStuff = "SELECT login, email, password FROM accounts";
 
             ResultSet rows = sqlState.executeQuery(selectStuff);
 
             while(rows.next()){
-                System.out.println(rows.getString("login"));
+                System.out.println("Login: " + rows.getString("login") + "    Email: " + rows.getString("email") +
+                        "    Password: " + rows.getString("password"));
             }
+
+            sqlState.executeUpdate("INSERT INTO accounts "+ "VALUES ('gafer', 'gafer@ya.ru', 'chashpasw', '0933994443', 'Huge', 'Mangus', 'Duglas', 'Krakow', '1996-04-23', 'M', null)");
         }
 
         catch (SQLException sqlEx) {
